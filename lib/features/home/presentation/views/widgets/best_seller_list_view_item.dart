e98c11cb3809cb35ca4b5282586b1d39ec22f84a
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-18 10:53:24 +0200

    Update text styles and add BestSellerListViewItem widget

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -28,16 +29,34 @@ class BestSellerListViewItem extends StatelessWidget {
           ),
           const SizedBox(width: 30),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.5,
-                child: const Text(
+                child: Text(
                   'Harry Potter and The Goblet of Fire',
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(
+                    fontFamily: kGtSectraFine,
+                  ),
                 ),
               ),
+              const SizedBox(height: 3),
+              const Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              Row(
+                children: [
+                  Text(
+                    '19.99',
+                    style: Styles.textStyle20.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
             ],
           )
         ],
