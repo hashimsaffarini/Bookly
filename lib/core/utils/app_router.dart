d5517f1b750d9510357aa0ab32a081aec0f7bc6c
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-19 00:31:56 +0200

    Add search feature and update navigation in AppRouter

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:bookly/features/search/presentation/views/search_view.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -8,6 +9,7 @@ abstract class AppRouter {
   static const kHomeView = '/HomeView';
   static const kSplashView = '/';
   static const kBookDetailsView = '/BookDetailsView';
+  static const kSearchView = '/SearchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -26,6 +28,12 @@ abstract class AppRouter {
         path: kBookDetailsView,
         pageBuilder: (context, state) => const MaterialPage(
           child: BookDetailsView(),
+        ),
+      ),
+      GoRoute(
+        path: kSearchView,
+        pageBuilder: (context, state) => const MaterialPage(
+          child: SearchView(),
         ),
       ),
     ],
