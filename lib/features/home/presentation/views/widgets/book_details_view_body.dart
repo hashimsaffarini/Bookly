d84c7767d23947aad69ebca57f3a3ffcacc2aeed
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-04-18 22:57:33 +0200

    Add CustomButton widget and update BookDetailsViewBody and BooksAction to use it

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -36,7 +37,9 @@ class BookDetailsViewBody extends StatelessWidget {
           const SizedBox(height: 18),
           const BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
-          )
+          ),
+          const SizedBox(height: 37),
+          const BooksAction(),
         ],
       ),
     );
